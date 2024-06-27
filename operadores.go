e21f335b28a6641dd2584e7d1b945ed969de8d10
Skrexz <xsebastianrojasx@gmail.com>
2024-06-27 09:30:44 -0500

    feat:

--- operadores.go
+++ operadores.go
@@ -27,4 +27,31 @@ func main() {
 	var modulo int = a % b //Resultado es 0 (Resto de la división)
 	fmt.Println(modulo)
 
+	//Operadores de Asignación
+
+	//1. Asignación Simple (=)
+	var x int
+	x = 10
+	fmt.Println(x)
+
+	//2. Asignación con Suma (+=)
+	x += 5 //Ahora x es 15
+	fmt.Println(x)
+
+	//3. Asignación con Resta (-=)
+	x -= 3 //Ahora x es 12
+	fmt.Println(x)
+
+	//4. Asignación con Multiplicación (*=)
+	x *= 2 //Ahora x es 24
+	fmt.Println(x)
+
+	//5. Asignación con División (/=)
+	x /= 4 //Ahora x es 6
+	fmt.Println(x)
+
+	//6. Asignación de Módulo (%=)
+	x %= 4 //Ahora x es 2
+	fmt.Println(x)
+
 }
