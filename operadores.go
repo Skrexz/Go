8c39d52e6f334a371b65e033ca38cec6a6bbee31
Skrexz <xsebastianrojasx@gmail.com>
2024-06-27 09:19:41 -0500

    Operadores

---
+++ operadores.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Operador suma (+)
+	var a int = 10
+	var b int = 5
+
+	var suma int = a + b //Resultado es 15
+	fmt.Println(suma)
+
+	//Operador resta (-)
+	var resta int = a - b //Resultado es 5
+	fmt.Println(resta)
+
+	//Operador Multiplicación (*)
+	var multiplicacion int = a * b //Resultado es 50
+	fmt.Println(multiplicacion)
+
+	//Operador división (/)
+	var division int = a / b //Resultado es 2 (División entera)
+	fmt.Println(division)
+
+	//Operador Modulo (%)
+	var modulo int = a % b //Resultado es 0 (Resto de la división)
+	fmt.Println(modulo)
+
+}
