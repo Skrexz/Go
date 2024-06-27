922b404335facf23ce78a690faca898fc66876c6
Skrexz <xsebastianrojasx@gmail.com>
2024-06-27 08:41:58 -0500

    Variables y HolaMundo

---
+++ variables.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//Variable de Entero
+	var edad int = 30
+	fmt.Println(edad)
+
+	//Variable Flotante
+	var altura float64 = 1.75
+	fmt.Println(altura)
+
+	//Variable cadena de Texto
+}
