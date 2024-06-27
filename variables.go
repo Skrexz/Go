04a5be3ecbcff214e257eb9d4816be60c2b64ed5
Skrexz <xsebastianrojasx@gmail.com>
2024-06-27 09:00:39 -0500

    Se añadieron más variables

--- variables.go
+++ variables.go
@@ -23,4 +23,26 @@ func main() {
 	//Variable Arreglo (Array)
 	var numeros [5]int = [5]int{1, 2, 3, 4, 5}
 	fmt.Println(numeros)
+
+	//Variable Slice
+	var frutas []string = []string{"Manzana,", "Pera,", "Naranja"}
+	fmt.Println(frutas)
+
+	//Variable Mapa (map)
+	var edades map[string]int = map[string]int{"Alice": 25, "Bob": 30}
+	fmt.Println(edades)
+
+	//Variable Estructura
+	type Persona struct {
+		Nombre string
+		Edad   int
+	}
+	var persona Persona = Persona{Nombre: "Sebastian", Edad: 25}
+	fmt.Println(persona)
+
+	//Variable Puntero
+	var puntero *int
+
+	//Variable interface
+	var i interface{} = "Texto"
 }
