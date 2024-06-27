68d6d163ebf4a723680feeab3bdb69ceacdcdaae
Skrexz <xsebastianrojasx@gmail.com>
2024-06-27 09:08:35 -0500

    Se añadió tipos de datos

---
+++ tipos-de-datos.go
@@ -0,0 +1,30 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	//int
+	var numero int = 42
+	fmt.Println(numero)
+
+	//float32
+	var precio float32 = 19.99
+	fmt.Println(precio)
+
+	//float64
+	var pi float64 = 3.14159
+	fmt.Println(pi)
+
+	//string
+	var saludo string = "hola, mundo"
+	fmt.Println(saludo)
+
+	//bool
+	var esActivo = false
+	fmt.Println(esActivo)
+
+	//byte (un alias para uint8, representa un byte)
+	var letra byte = 'A'
+	fmt.Println(letra)
+}
